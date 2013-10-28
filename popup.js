9c897ae1abb63fd5f22e4941181ce6a3da6b6593
Oleg Puzanov <oleg.puzanov@gmail.com>
2013-10-28 10:18:36 +0200

    Project init

---
+++ popup.js
@@ -0,0 +1,113 @@
+// Copyright (c) Oleg Puzanov (http://olegpuzanov.com/)
+
+var CurrencyConvertor = {
+  /**
+   * @type {string}
+   * @private
+   */
+  url: 'http://www.google.com/ig/calculator?q=%amount%%from%=?%to%',
+
+  /**
+   * @public
+   */
+  getRate: function() {
+    var amount = document.getElementById('amount').value;
+    var from = document.getElementById('from').value;
+    var to = document.getElementById('to').value;
+
+    // clear results container by setting blank space to avoid content jumping
+    document.getElementById('result').innerHTML = '&nbsp;';
+
+    // save amount, from and to values to local stogare
+    chrome.storage.local.set({'amount': amount});
+    chrome.storage.local.set({'from': from});
+    chrome.storage.local.set({'to': to});
+
+    // set parameters for api url
+    var url = this.url.replace('%amount%', amount)
+                      .replace('%from%', from)
+                      .replace('%to%', to);
+    
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.onload = this.showResult.bind(this);
+    req.send(null);
+  },
+
+  /**
+   * @param {ProgressEvent} e The XHR ProgressEvent.
+   * @private
+   */
+  showResult: function (e) {
+    // add double quotes to keys to get valid json and then parse JSON
+    var response = JSON.parse(e.target.responseText.replace('lhs', '"lhs"')
+                                                   .replace('rhs', '"rhs"')
+                                                   .replace('error', '"error"')
+                                                   .replace('icc', '"icc"'));
+    document.getElementById('result').innerHTML = response.rhs;
+  },
+
+  /**
+   * Validate amount input
+   * @public
+   */
+   validate: function(e) {
+    var evnt = e || window.event;
+    var key = evnt.keyCode || evnt.which;
+    key = String.fromCharCode(key);
+    var regex = /[0-9]|\./;
+    if( !regex.test(key) ) {
+      evnt.returnValue = false;
+      if(evnt.preventDefault) evnt.preventDefault();
+    }
+   }
+};
+
+// attach validation to amount field
+document.getElementById('amount').addEventListener('keypress', function () {
+  CurrencyConvertor.validate(event);
+});
+
+// load saved "amount", "from" and "to" values (if any) from local storage 
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.storage.local.get('amount', function (result) {
+    if ( undefined != result.amount ) {
+      document.getElementById('amount').value = result.amount;
+    }
+  });
+
+  chrome.storage.local.get('from', function (result) {
+    if ( undefined != result.from ) {
+      var from = document.getElementById("from");
+      for(var i, j = 0; i = from.options[j]; j++) {
+          if(i.value == result.from) {
+              from.selectedIndex = j;
+              break;
+          }
+      }     
+    }
+  });
+
+  chrome.storage.local.get('to', function (result) {
+    if ( undefined != result.to ) {
+      var to = document.getElementById("to");
+      for(var i, j = 0; i = to.options[j]; j++) {
+          if(i.value == result.to) {
+              to.selectedIndex = j;
+              break;
+          }
+      }     
+    }
+  }); 
+});
+
+// attach events to button "Convert" click and "Amount" submit event
+document.getElementById('convertBtn').addEventListener('click', function () {
+  CurrencyConvertor.getRate();
+});
+
+document.getElementById('amount').addEventListener('keypress', function (event) {
+  if ( event.keyCode == 13 ) {
+    CurrencyConvertor.getRate();
+  }
+});
